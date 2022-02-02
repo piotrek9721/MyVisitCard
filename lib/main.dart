6563891b3779caf8083fff5090e0cfdbb00da3c6
piotrekkruk <piotrekkruk@icloud.com>
2022-02-02 13:55:36 +0100

    FINAL VISIT CARD

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,77 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey.shade500,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.amber,
-                  child: Text('Container 3')),
-              Container(
-                  width: 100.0,
-                  height: 50.0,
-                  color: Colors.blue,
-                  child: Text('Container 2')),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 3'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/me.jpg'),
               ),
+              Text(
+                'Piotr Kruk',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                ' Data Engineer ',
+                style: TextStyle(
+                  fontFamily: 'RobotoMono',
+                  fontSize: 20.0,
+                  color: Colors.blueGrey.shade900,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.blueGrey.shade900,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey.shade900,
+                  ),
+                  title: Text(
+                    '+49 (0172) 6738036',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade900,
+                      fontFamily: 'RobotoMono',
+                      fontSize: 20.0,
+                      letterSpacing: 0.3,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.blueGrey.shade900,
+                    ),
+                    title: Text(
+                      'piotrekkruk@icloud.com',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blueGrey.shade900,
+                          fontFamily: 'RobotoMono'),
+                    ),
+                  )),
             ],
           ),
         ),
